4fe196e4d13025c3012ce49e2fa970aaaa54168b
UhJin Lee <dldjwls3@snu.ac.kr>
2020-07-07 14:42:01 +0900

    minor fix

--- train.py
+++ train.py
@@ -1,20 +1,28 @@
 from pytorch_lightning import Trainer, seed_everything
 from argparse import ArgumentParser
 from baseline import Baseline
-from comet_ml import Experiment
+from comet_ml import Optimizer
 from pytorch_lightning.logging import CometLogger
 from dataset import KOSPI200Dataset
+import hydra
+from omegaconf import DictConfig
 
 seed_everything(1)
-KOSPI200Dataset.set_data_split(500, 30, 30)
+
+# @hydra.main(config_path='./hydra/config.yaml')
+# def parse_config(cfg: DictConfig):
+#     print(cfg.pretty())
+#
 
 if __name__ == '__main__':
+
+    KOSPI200Dataset.set_data_split(500, 30, 585)  # 2020.07.03 615일(개장일 기준) 이전이 2018.01.02
+
     comet_logger = CometLogger(
         api_key="SWhvV0XPkHV8tPdU8Nv67EXxU",
         workspace="dldjwls3",  # Optional
-        project_name="stock_playground",  # Optional
-        # rest_api_key=os.environ["COMET_REST_KEY"], # Optional
-        experiment_name="default" # Optional
+        project_name="stock-gcn-experiment",
+        experiment_name='test'
     )
 
     model = Baseline()
